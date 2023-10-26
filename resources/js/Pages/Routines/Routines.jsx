8ddbe0077d2c785f82bcfbe8678ec32ba3bdd7e9
Muhammad-Yuda-Yudistira <yudistira22112022@gmail.com>
2023-10-26 18:12:11 +0700

    chore(init) Install and Configuration

---
+++ resources/js/Pages/Routines/Routines.jsx
@@ -0,0 +1,9 @@
+import { Link, Head } from "@inertiajs/react";
+
+export default function Routines() {
+    return (
+        <>
+            <button className="btn btn-primary">Primary</button>
+        </>
+    );
+}
